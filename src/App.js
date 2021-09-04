5a3b9dbd00b6918e49a5d30ee2ee5d8803a713fe
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-04 18:01:35 +0530

    Simple React Player Created. Youtube URL.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import {Container, Typography} from "@material-ui/core";
+import ReactPlayer from "react-player";
 function App() {
   return (
-    <div className="App">
-
-    </div>
+      <React.Fragment>
+        <AppBar position="fixed">
+          <Toolbar>
+            <Typography variant="h6">React Video Player</Typography>
+          </Toolbar>
+        </AppBar>
+        <Toolbar/>
+        <Container maxWidth="md">
+            <ReactPlayer
+            url="https://www.youtube.com/watch?v=wGixQPuG1GY"
+            muted={false}
+            playing={true}
+            />
+        </Container>
+      </React.Fragment>
   );
 }
 
